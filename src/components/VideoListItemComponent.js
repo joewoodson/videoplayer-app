bb17ff6ace564c59c5f616fcb0c7436a7af965b7
Joe <contact@joewoodson.de>
2016-01-15 18:32:07 +0100

    add thumbnails and titles to video list

--- src/components/VideoListItemComponent.js
+++ src/components/VideoListItemComponent.js
@@ -1,21 +1,30 @@
 'use strict';
 
 import React from 'react';
-import Row from 'react-bootstrap/lib/Row';
+import Panel from 'react-bootstrap/lib/Panel';
 import Col from 'react-bootstrap/lib/Col';
+import Image from 'react-bootstrap/lib/Image';
 
 require('styles/VideoListItem.sass');
 require('bootstrap/dist/css/bootstrap.css');
 
 const VideoListItemComponent = ({video}) => {
-	console.log(video);
+	const imageUrl = video.snippet.thumbnails.default.url;
 
 	return (
-		// <li>Video</li>
-		<Row>
-				<Col lg={4} className="purple">Left Side</Col>
-				<Col lg={8} className='yellow'>Right Side</Col>
-		</Row>
+		<Col className='video-listing' sm={4} smOffset={8}>
+			<Panel>
+				<div>
+				 <Col md={5}>
+				 	<Image src={imageUrl} thumbnail />
+				 </Col>
+
+				 <Col md={7}>
+				 	<div className='video-title'>{video.snippet.title}</div>
+				 </Col>
+				</div>
+			</Panel>
+		</Col>
 	)
 }
 
